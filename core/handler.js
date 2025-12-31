8299992c1066a0c93ab849824b6488e3b03ee1a9
chenblacker <chenblacker@gmail.com>
2025-12-31 12:50:30 +0800

    Update handler.js

--- core/handler.js
+++ core/handler.js
@@ -709,7 +709,7 @@ function getConfigLink(uuid, host, address, port, remarks, proxyip, protType, na
     if (s5) {
         pathParm = pathParm + `&S5=${s5}`;
     }
-    let path = `/?ed=2560` + pathParm;
+    let path = `/?ed=2048` + pathParm;
     const fp = 'randomized';
     let tls = ['tls', true];
     if (host.includes('.workers.dev') || host.includes('pages.dev')) {
